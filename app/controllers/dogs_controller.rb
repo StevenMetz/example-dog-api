44b7fb96dbbd9090ff01b2ffdab56f31a3adf8ba
StevenMetz <stevenm.codes@gmail.com>
2022-11-10 16:03:46 -0600

    added all CRUD and only user can CRUD but anyone can view dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,4 +1,6 @@
 class DogsController < ApplicationController
+  before_action :authenticate_user, except: [:index]
+
   def index
     dogs = Dog.all
     render json: dogs.as_json
@@ -9,12 +11,31 @@ class DogsController < ApplicationController
       name: params[:name],
       breed: params[:breed],
       age: params[:age],
-      user_id: current_user.id
+      user_id: current_user.id,
     )
     if dog.save
       render json: dog
     else
       render json: { message: "dog didnt save" }
     end
+  end
+
+  def update
+    dog = Dog.find_by(id: params["id"])
+    dog.name = params["name"] || dog.name
+    dog.breed = params["breed"] || dog.breed
+    dog.age = params["age"] || dog.age
+    @dog = dog
+    if product.save
+      render json: dog
+    else
+      render json: { errors: product.errors.full_messages }, status: 418
+    end
+  end
+
+  def delete
+    dog = Dog.find_by(id: params["id"])
+    dog.destroy
+    render json: { message: "Dog Removed" }
   end
 end
